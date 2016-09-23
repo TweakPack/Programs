29d330f1fed0e16cedaa575a6602c86a73e90c90
Ariaan Bruinsma <ariaanbruinsma@gmail.com>
2016-09-23 11:47:08 +0200

    Code overhaul
    Cleaned up the code, implemented code conventions, added (usable) comments, improved certain loops, removed unnecessary redraws, faster calculation and added support for Capacitor Banks

--- histogram/histogram.lua
+++ histogram/histogram.lua
@@ -1,115 +1,217 @@
--- coded by zgubilembulke
--- modified by McBrown (september 2016)
- 
+--[[
+@title Energy Storage Monitor & Regulator
+@author: zgubilembulke (november 2014)
+@author: McBrown (september 2016)
+@description: This script generates 3 horizontal graphs, that shows the
+    power input, -output & -stored.
+]]
+
+
+--[[
+Require's
+]]
 local comp = require('component');
 local sides = require('sides');
- 
+
+
+--[[
+Constants
+]]
+local STORAGE_CAPACITOR_BANK   = 0x0001
+local STORAGE_INDUCTION_MATRIX = 0x0002
+local POWER_EU = 0x0001
+local POWER_RF = 0x0002
+
+--[[
+Config
+@note:
+    Limits are in percentages (e.g. 1.0 = 100%, 0.0 = 0%) where the lua's float
+    precision limit is allowed 
+]]
 local drainLimit  = .14
 local chargeLimit = .14025
- 
-local rsIoBlock = comp.redstone -- default
-local rsIoSide  = sides.left -- (top, bottom, front, back, left, right)
- 
--- Don't change below this line (unless you know what you're doing) --
- 
+
+local storageDevice      = STORAGE_INDUCTION_MATRIX
+local capacitorBankCount = 25
+local powerUnits         = POWER_RF
+
+local resX, resY       = 160, 40
+local refreshRate      = 1 --screen update frequency(seconds)
+
+local precisionDisplay = 100
+
+local rsIoBlock = comp.redstone
+local rsIoSide  = sides.left
+
+--[[ Don't change below this line (unless you know what you're doing) ]]
+
 local gpu = comp.gpu
-local matrix = comp.induction_matrix
-local isCharging = false
-local refresh_rate = 1 --screen update frequency(seconds)
- 
-gpu.setResolution(160,40) -- set resolution
-w,h = gpu.getResolution()
- 
-function round(t)
-  return math.floor(t*100)*0.01
+
+--[[
+@description: Rounds a number with a certain precision
+@note:
+    e.g. number is 24.5635024
+    precision 1000 will turn to 24.563
+    precision 100 will turn to 24.56
+    precision 10 will turn to 24.5
+]]
+function round(number, precision)
+    return math.floor(number * precision) / precision
 end
- 
-function max_array_value(array)
-  local max = 0
-  for k, v in pairs(array) do
-      if v > max then
-          max = v
-      end
-  end
-  return max
-end
- 
-function min_array_value(array)
-  local min = array[1]
-  for k, v in pairs(array) do
-      if v < min then
-          min = v
-      end
-  end
-  return min
-end
- 
-function tablelen(array)
-  local count = 0
-  for _ in pairs(array) do count = count + 1 end
-  return count
-end
- 
-function shorter_number(num)
-  if num <= 1000 then
-    return tostring(num)
-  elseif num <= 1000000 then
-    return tostring(round(num/1000)) .. "k"
-  elseif num <= 1000000000 then
-    return tostring(round(num/1000000)) .. "M"
-  elseif num <= 1000000000000 then
-    return tostring(round(num/1000000000)) .. "G"
-  elseif num <= 1000000000000000 then
-    return tostring(round(num/1000000000000)) .. "T"
-  else
-    return '';
-  end
-end
- 
-function graph_horizontal(xpos,ypos,g_width,g_height,array,addbars_bool,detailed_bool,points_color,lines_color,text_color,bg_color)
-  local maxvalue = max_array_value(array)
-  local minvalue = 0
-  if detailed_bool == true then
-    minvalue = min_array_value(array)
-  end
- 
-  local arraylen = tablelen(array)
-  gpu.setBackground(bg_color)
-  gpu.fill(xpos,ypos,g_width,g_height,' ')
-  gpu.setBackground(lines_color)
-  gpu.fill(xpos+7,ypos,1,g_height," ") -- change by McB
-  gpu.fill(xpos,ypos+g_height-1,g_width,1," ")  
-  gpu.setBackground(bg_color)
-  gpu.setForeground(text_color)
- -- gpu.set(xpos+((g_width/2)-(string.len(labelText)/2)), ypos-1, tostring(labelText))                    -- place label
-  gpu.set(xpos,ypos+1,tostring(shorter_number(maxvalue)))                                               -- top value display
-  gpu.set(xpos,ypos+1+(g_height-2)/2,tostring(shorter_number(minvalue+((maxvalue-minvalue)/2))) )              -- mid value display
-  gpu.set(xpos,ypos-1+g_height-1,tostring(shorter_number(minvalue)) )                                                                  -- 0 value display
-  gpu.setBackground(points_color)                                                                             --graph color
-    if arraylen > g_width-8 then -- change by McB
-      table.remove(array,1)
+
+
+--[[
+@description: Determines the largest number within an array
+]]
+function maxArrayValue(inputArray)    
+    if inputArray == nil then
+        return 0
     end
- 
-  if addbars_bool == true then
-    for a,b in pairs(array)do
-      local yp=math.floor(ypos+1+(g_height-3)-(((b-minvalue)/(maxvalue-minvalue))*(g_height-3)))
-      gpu.fill(xpos+7+a,yp,1,ypos+g_height-yp-1," ")    -- vertical bars version -- changed by McB
+    
+    local max = inputArray[1]
+
+    for _, value in pairs(inputArray) do
+        if value > max then
+            max = value
+        end
     end
-  elseif addbars_bool ~= true then
-    for a,b in pairs(array)do
-      local yp=math.floor(ypos+1+(g_height-3)-(((b-minvalue)/(maxvalue-minvalue))*(g_height-3)))
-      gpu.fill(xpos+7+a,yp,1,1," ")  -- single point version -- changed by McB
+   
+    return max
+end
+
+
+--[[
+@description: Determines the smallest number within an array
+]]
+function minArrayValue(inputArray)    
+    if inputArray == nil then
+        return 0
     end
-  end
+        
+    local min = inputArray[1]
+
+    for _, value in pairs(inputArray) do
+        if value < min then
+            min = value
+        end
+    end
+    
+    return min
 end
- 
-function drawLabel(xPos, yPos, width, bgColor, textColor, textLabel)
-  gpu.setBackground(bgColor)
-  gpu.setForeground(textColor)
-  gpu.fill(xPos, yPos, width, 1, " ")
-  gpu.set(xPos+((width/2)-(string.len(textLabel)/2)), yPos, tostring(textLabel))
+
+
+--[[
+@description: Determines the size of an array 
+]]
+function tableLen(inputArray)
+    local count = 0
+    
+    if inputArray ~= nil then
+        for value in pairs(inputArray) do
+            count = count + 1
+        end
+    end
+    
+    return count
 end
- 
+
+
+--[[
+@description: Shortens, rounds and makes a number humanly readable
+]]
+function shortenNumber(inputNumber)
+    local correctionTable = {
+       {letter = '',  correction = 1},
+       {letter = 'k', correction = 1000},
+       {letter = 'M', correction = 1000000},
+       {letter = 'G', correction = 1000000000},
+       {letter = 'T', correction = 1000000000000},
+       {letter = 'P', correction = 1000000000000000}}
+    
+    for _, checkSet in ipairs(correctionTable) do
+        if inputNumber <= checkSet.correction * 1000 then
+            return tostring(round(inputNumber / checkSet.correction, precisionDisplay)) .. checkSet.letter
+        end
+    end
+
+    return tostring(inputNumber)
+end
+
+--[[
+@description: Draws a horizontal graph that grows dynamically to the size of the canvas
+]]
+function graphHorizontal(xPos, yPos, graphWidth, graphHeight, inputArray, addBars, detailed, pointsColor, linesColor, textColor, bgColor)
+    local maxValue = maxArrayValue(inputArray)
+    local minValue = 0
+    local arrayLen = tableLen(inputArray)
+    local legendWidth = 8 
+    
+    -- Determine the level of detail
+    if detailed == true then
+        minValue = minArrayValue(inputArray)
+    end
+
+    -- Draw canvas
+    gpu.setBackground(bgColor)
+    gpu.fill(xPos, yPos, graphWidth, graphHeight, ' ')
+    
+    -- Draw border lines
+    gpu.setBackground(linesColor)
+    gpu.fill(xPos + legendWidth, yPos, 1, graphHeight, ' ')
+    gpu.fill(xPos, yPos + graphHeight - 1, graphWidth, 1, ' ')
+    
+    -- Draw & determine legend
+    gpu.setBackground(bgColor)
+    gpu.setForeground(textColor)
+    
+    -- top value legend
+    gpu.set(xPos, yPos + 1, shortenNumber(maxValue))
+    
+    -- mid value legend
+    gpu.set(xPos, yPos + 1 + (graphHeight - 2) / 2, shortenNumber(minValue + ((maxValue - minValue) / 2)))
+
+     -- bottom value legend
+    gpu.set(xPos, yPos - 1 + graphHeight - 1, shortenNumber(minValue))
+    
+    -- Start drawing
+    local tempYPos = 0
+    local tempFillerHeight = 1
+    
+    -- The color used to paint the background of the space-char
+    gpu.setBackground(pointsColor)
+    
+    -- Shift the array
+    if arrayLen > graphWidth - (legendWidth + 1) then
+        table.remove(inputArray, 1)
+    end
+
+    -- Draw points/bars
+    for key, value in pairs(inputArray) do
+        tempYPos = math.floor(yPos + 1 + (graphHeight - 3) - (((value - minValue) / (maxValue - minValue)) * (graphHeight - 3)))
+    
+        if addBars == true then
+            tempFillerHeight = yPos + graphHeight - tempYPos - 1
+        end
+    
+        gpu.fill(xPos + legendWidth + key, tempYPos, 1, tempFillerHeight, ' ')
+    end
+end
+
+
+--[[
+@description: Draws a label on a specified location, text is always centered
+]]
+function drawLabel(xPos, yPos, labelWidth, bgColor, textColor, textLabel)
+    gpu.setBackground(bgColor)
+    gpu.setForeground(textColor)
+    gpu.fill(xPos, yPos, labelWidth, 1, ' ')
+    gpu.set(xPos + ((labelWidth / 2) - (string.len(textLabel) / 2)), yPos, tostring(textLabel))
+end
+
+
+--[[
+@description: sets charging-state and toggles the Redstone IO block 
+]]
 function setCharging(charging)
   isCharging = charging
  
@@ -119,71 +221,115 @@ function setCharging(charging)
     rsIoBlock.setOutput(rsIoSide, 0)
   end
 end
- 
+
+
+--[[
+Script start
+]]
+isCharging     = false
+
+local storageObj     = nil
+local capacity       = 0
+local previousEnergy = 0
+local powerCalc      = 1
+local capacity       = 0
+local storedLabel    = ''
+
+inputArray  = {}
+outputArray = {}
+storedArray = {}
+
+-- Determine power-units for powerUnit & -mod calculations
+if storageDevice == STORAGE_CAPACITOR_BANK
+    and powerUnits == POWER_RF then
+        powerCalc = powerCalc
+elseif storageDevice == STORAGE_CAPACITOR_BANK
+    and powerUnits == POWER_EU then
+        powerCalc = powerCalc * .4
+elseif storageDevice == STORAGE_INDUCTION_MATRIX
+    and powerUnits == POWER_RF then
+        powerCalc = powerCalc * .4
+elseif storageDevice == STORAGE_INDUCTION_MATRIX
+    and powerUnits == POWER_EU then
+        -- Do absolutely nothing
+end
+
 setCharging(false)
- 
-array1 = {}
-array2 = {}
-array3 = {}
- 
---local previousEnergy = 0
-local capacity = matrix.getMaxEnergy() * .4
- 
-repeat -- Begin loop
- 
-input     = matrix.getInput() * .4
-output    = matrix.getOutput() * .4
-stored    = matrix.getEnergy() * .4
-storedPrc = stored / capacity
- 
---stored = comp.tile_blockcapacitorbank_name.getEnergyStored() * capacitorBankCount
---if previousEnergy > stored then
---  input   = 0
---  output  = previousEnergy - stored
---elseif previousEnergy < stored then
---  input   = stored - previousEnergy
---  output  = 0
---else
---  input   = 0
---  output  = 0
---end
- 
-table.insert(array1, input)
-table.insert(array2, output)
-table.insert(array3, stored)
- 
-gpu.setBackground(0x141414) -- main background color
-gpu.fill(1,1,w,h,' ')
- 
-graph_horizontal(2,5,78,17,array1,true,true,0x66CC00,0xFFFFFF,0xFFFFFF,0x333333)
-graph_horizontal(82,5,78,17,array2,true,false,0xFF3232,0xFFFFFF,0xFFFFFF,0x333333)
-graph_horizontal(2,24,158,13,array3,true,true,0x2E87D8,0xFFFFFF,0xFFFFFF,0x333333)
- 
-drawLabel(2, 4, 78, 0x222222, 0xFFFFFF, 'INPUT: ' .. shorter_number(input) .. 'RF')
-drawLabel(82, 4, 78, 0x222222, 0xFFFFFF, 'OUTPUT: ' .. shorter_number(output) .. 'RF')
- 
-storedLabel = 'STORED: ' .. shorter_number(stored) .. 'RF (' .. round(storedPrc*100) .. '%)'
- 
-if isCharging then
-  drawLabel(2, 23, 158, 0x222222, 0x66CC00, storedLabel .. ' - CHARGING')
-else
-  drawLabel(2, 23, 158, 0x222222, 0xFFFFFF, storedLabel)
+
+-- Set resolution
+gpu.setResolution(resX, resY)
+
+-- Determine storage device
+if storageDevice == STORAGE_CAPACITOR_BANK then
+    storageObj = comp.tile_blockcapacitorbank_name
+    capacity   = storageObj.getMaxEnergyStored() * capacitorBankCount * powerCalc
+elseif storageDevice == STORAGE_INDUCTION_MATRIX then
+    storageObj = comp.induction_matrix
+    capacity   = storageObj.getMaxEnergy() * powerCalc
 end
- 
---previousEnergy = stored
- 
--- Check and toggle charging
- 
-drawLabel(1, 1, 158, 0x222222, 0xFFFFFF, 'Olle grieze!!!');
- 
--- Debug label
--- drawLabel(1, 1, 158, 0x000000, 0xFFFFFF, 'Debug: isCharging:' .. tostring(isCharging) .. ', storedPrc:' .. tostring(storedPrc) .. ', drainLimit:' .. drainLimit)
- 
-if isCharging == false and storedPrc <= drainLimit then
-  setCharging(true)
-elseif isCharging == true and storedPrc >= chargeLimit then
-  setCharging(false)
-end
- 
-os.sleep(refresh_rate)
+
+-- Draw top label
+drawLabel(1, 1, 160, 0x222222, 0xFFFFFF, 'ENERGY MONITOR')
+
+-- Draw legend
+drawLabel(1, resY, 160, 0x222222, 0x969696, '(Drain limit: ' .. round(drainLimit * 100, 100) .. '%, Charge limit: ' .. round(chargeLimit * 100, 100) .. '%)')
+
+--[[
+Begin application loop
+]]
+repeat
+    -- Determine storage device
+    if storageDevice == STORAGE_CAPACITOR_BANK then
+        stored = storageObj.getEnergyStored() * capacitorBankCount * powerCalc 
+        if previousEnergy > stored then
+              input  = 0
+              output = previousEnergy - stored
+        elseif previousEnergy < stored then
+              input  = stored - previousEnergy
+              output = 0
+        else
+              input  = 0
+              output = 0
+        end
+        previousEnergy = stored
+    elseif storageDevice == STORAGE_INDUCTION_MATRIX then
+        input  = storageObj.getInput() * powerCalc
+        output = storageObj.getOutput() * powerCalc
+        stored = storageObj.getEnergy() * powerCalc
+    end
+
+    storedPrc = stored / capacity
+
+    table.insert(inputArray, input)
+    table.insert(outputArray, output)
+    table.insert(storedArray, stored)
+
+    gpu.setBackground(0x141414) -- main background color
+    gpu.fill(1, 2, resX, resY - 2, ' ')
+
+    graphHorizontal(2, 4, 78, 17, inputArray, true, false, 0x66CC00, 0xFFFFFF, 0xFFFFFF, 0x333333)
+    graphHorizontal(82, 4, 78, 17, outputArray, true, false, 0xFF3232, 0xFFFFFF, 0xFFFFFF, 0x333333)
+    graphHorizontal(2, 23, 158, 16, storedArray, false, true, 0x2E87D8, 0xFFFFFF, 0xFFFFFF, 0x333333)
+
+    drawLabel(2, 3, 78, 0x222222, 0xFFFFFF, 'INPUT: ' .. shortenNumber(input) .. 'RF')
+    drawLabel(82, 3, 78, 0x222222, 0xFFFFFF, 'OUTPUT: ' .. shortenNumber(output) .. 'RF')
+
+    storedLabel = shortenNumber(stored) .. 'RF (' .. round(storedPrc * 100, 100) .. '%)';
+    
+    if isCharging == true then
+        drawLabel(2, 22, 158, 0x222222, 0x66CC00, storedLabel .. ' - CHARGING')
+    else
+        drawLabel(2, 22, 158, 0x222222, 0xFFFFFF, storedLabel)
+    end
+
+    -- Check and toggle charging state
+    if isCharging == false and storedPrc <= drainLimit then
+        setCharging(true)
+    elseif isCharging == true and storedPrc >= chargeLimit then
+        setCharging(false)
+    end
+    
+    -- Don't loop like a truck beserker
+    os.sleep(refreshRate)
+    
 until false
